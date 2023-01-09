1d58283008e032ca914b7b880b710b62f347ee37
Frcru <91342407+Frcru@users.noreply.github.com>
2023-01-09 17:37:42 -0500

    Copia de datos solo main

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:system_delivery/application/use_case/frm_Login.dart';
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: frmLogin(),
+    );
+  }
+}
